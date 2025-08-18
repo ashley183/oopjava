be03bed86a8ecb4d26a3f5d70a06eaa2234d5a6c
ashley183 <ashleymaemorales694@gmail.com>
2025-08-18 14:49:43 +0800

    Add files via upload

---
+++ exercises/Problem6.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exercises;
+
+/**
+ *
+ * @author User
+ */
+public class Problem6 {
+    public static void main(String[] args) {
+       
+        // Test data
+        int num1 = 125;
+        int num2 = 24;
+        
+        // Perform operations
+        int sum = num1 + num2;
+        int difference = num1 - num2;
+        int product = num1 * num2;
+        int quotient = num1 / num2;
+        int remainder = num1 % num2;
+        
+        
+        // Display results
+        System.out.println(num1 + " + " + num2 + " = " + sum);
+        System.out.println(num1 + " - " + num2 + " = " + difference);
+        System.out.println(num1 + " % " + num2 + " = " + product);
+        System.out.println(num1 + " / " + num2 + " = " + quotient);
+        System.out.println(num1 + " main " + num2 + " = " + remainder);
+        
+        
+        
+        
+    }
+    
+}
